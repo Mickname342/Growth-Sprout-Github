0b27747cbd65ba83d6ce1dbc55ce32dcfc2a3958
Mickname342 <michele.marchese.andreu@gmail.com>
2023-02-01 12:38:51 +0000

    Bramble kills player
    The bramble now kills the player by restarting the scene

--- Growth Sprout/Assets/Scripts/PlayerController.cs
+++ Growth Sprout/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -64,4 +65,13 @@ public class PlayerController : MonoBehaviour
             return false;
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bramble"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
 }
