bb387f8c19ce81e8f1216a21f04be715423dfdb1
Mickname342 <michele.marchese.andreu@gmail.com>
2023-02-02 11:38:54 +0000

    Pickup now works correctly
    Now you can only pick up one seed at a time

--- Growth Sprout/Assets/Scripts/CollectScript.cs
+++ Growth Sprout/Assets/Scripts/CollectScript.cs
@@ -12,10 +12,12 @@ public class CollectScript : MonoBehaviour
     public UnityEvent OutsidePot;
     public Transform seed;
     public Transform Player;
+    public GameObject PlayerTagger;
     public bool collectable = false;
     public bool collected = false;
     public float offset = 0.5f;
     public bool picked = false;
+    public bool droppedThisFrame = false;
 
     private void Update()
     {
@@ -26,30 +28,15 @@ public class CollectScript : MonoBehaviour
         if (collected == true && Input.GetKey(KeyCode.DownArrow))
         {
             collected = false;
-            //picked = false;
-            //Change.Invoke();
-        }
-        if (collected == false)
-        {
             Change.Invoke();
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collectable == true && collision.CompareTag("Player") && !picked)
+        if (collectable == true && collision.CompareTag("Player"))
         {
             NotChange.Invoke();
             collected = true;
-            //picked = true;
         }
-    }
-    public void AlreadyPicked()
-    {
-        picked = true;
-    }
-
-    public void FreeToPick()
-    {
-        picked = false;
     }
 }
